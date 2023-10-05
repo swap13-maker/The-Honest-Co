c81f126735d310fdaa9b6c44be42c5277f884a98
Raj Soni <rajsoni19619@gmail.com>
2023-10-05 23:18:17 +0530

    [feat]: portfolio zoom effect

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -32,17 +32,6 @@ const Portfolio = () => {
     setFilteredIndex(0); // Reset the index
   };
 
-  useEffect(() => {
-    AOS.init({
-      duration: 800, // Animation duration in milliseconds
-      offset: 200, // Offset (in pixels) from the element's position to trigger the animation
-      easing: "ease-in-out", // Animation easing (CSS transition-timing-function)
-      delay: 0, // Delay (in milliseconds) before the animation starts
-      once: true, // Whether the animation should occur only once or every time the element is scrolled into view
-      mirror: false, // Whether elements with the same data-aos value should animate individually or together
-    });
-  }, []);
-
   return (
     <section>
       {/* Header */}
@@ -185,33 +174,33 @@ const Portfolio = () => {
                     key={index}
                   >
                     <div className="border__fade">
-                      <div class="wrapper" data-aos="fade-up">
+                      <div class="wrapper">
                         <div class="zoom-effect-container">
                           <div class="image-card">
                             <img className="card-img-top" src={image} />
                           </div>
-                        </div>
-                        <div className="pb-5">
-                          <div className="blog-heading">
-                            {name}
-                            <svg
-                              width="20"
-                              height="14"
-                              viewBox="-10 0 25 14"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                id="Vector"
-                                d="M1.80778 13.8701L1.81026 13.8657L2.14145 13.5492L2.80382 12.9161L13.4018 2.78909L13.2658 8.77368L14.6105 8.80423L14.7986 0.523957L6.51835 0.335805L6.48779 1.68051L12.4724 1.81649L1.87444 11.9435L1.21207 12.5764L0.880888 12.8929L1.80778 13.8701Z"
-                                fill="black"
-                              />
-                            </svg>
+                          <div className="pb-5 translate__down">
+                            <div className="blog-heading">
+                              {name}
+                              <svg
+                                width="20"
+                                height="14"
+                                viewBox="-10 0 25 14"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  id="Vector"
+                                  d="M1.80778 13.8701L1.81026 13.8657L2.14145 13.5492L2.80382 12.9161L13.4018 2.78909L13.2658 8.77368L14.6105 8.80423L14.7986 0.523957L6.51835 0.335805L6.48779 1.68051L12.4724 1.81649L1.87444 11.9435L1.21207 12.5764L0.880888 12.8929L1.80778 13.8701Z"
+                                  fill="black"
+                                />
+                              </svg>
+                            </div>
+                            <div className="blog-title blog-hide w-75">
+                              {description}
+                            </div>
+                            <img className="blog-logo blog-hide" src={logo} />
                           </div>
-                          <div className="blog-title blog-hide w-75">
-                            {description}
-                          </div>
-                          <img className="blog-logo blog-hide" src={logo} />
                         </div>
                       </div>
                     </div>
