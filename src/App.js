7075ca56b945a792ab47b090a69f8c4af45c8fd1
gaybi77 <krishnagabale3@gmail.com>
2023-11-04 17:42:46 +0530

    Added Article pages and box line css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Portfolio from './pages/Portfolio.js';
 import Resource from './pages/Resource.js';
 import Project1 from './components/Portfolio/Project1.js';
 import Article1 from './components/Articles/Article1.js';
+import Article2 from './components/Articles/Article2';
+import Article3 from './components/Articles/Article3';
 
 function App() {
   return (
@@ -19,6 +21,9 @@ function App() {
         <Route path="/resources" element={<Resource />} />
         <Route path="/portfolio/project1" element={<Project1 />} />
         <Route path="/resources/articles/article1" element={<Article1 />} />
+        <Route path="/resources/articles/article2" element={<Article2 />} />
+        <Route path="/resources/articles/article3" element={<Article3 />} />
+
       </Routes>
     </Router>
   );
