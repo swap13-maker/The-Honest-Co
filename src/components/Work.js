d7f3e28bc0bd0fc53d56f519c7ba543b8e8fc28e
gaybi77 <krishnagabale3@gmail.com>
2023-12-01 15:04:34 +0530

    image link on our work page

--- src/components/Work.js
+++ src/components/Work.js
@@ -43,69 +43,69 @@ function Work() {
           D2C & E-Commerce · Mobile Apps · Online Store · Swift Order Tracking 
           </div>
         </Link>
-        <div className="gallery__item media-w-100 d-block">
+        <Link to="/portfolio/p2p-money-remittance-mobile-application" className="gallery__item media-w-100 d-block">
           <img src={webwork2} alt="peer to peer money remittance app"/>
           <Link to="/portfolio/p2p-money-remittance-mobile-application"><div className="work-heading">P2P Money Remittance Mobile Application </div>
           <div className="work-subheading">
           Banking & Finance · Mobile Apps · P2P Payments 
           </div></Link>
-        </div>
+        </Link>
       </div>
       <div className="gallery pb-md-5 pb-5 mobile-d-none">
-        <div className="gallery__item gallery__item--lg2 media-w-100 d-block">
+        <Link to="/portfolio/cab-hailing-travel-itinerary-platform" className="gallery__item gallery__item--lg2 media-w-100 d-block">
           <img src={webwork3} alt="Travel and itinerary app with cab booking "/>
           <Link to="/portfolio/cab-hailing-travel-itinerary-platform"><div className="work-heading">Travel & Cab Hailing Platform </div>
           <div className="work-subheading">
           Travel & Hospitality · Custom Softwares · Cab Hailing Platform 
           </div></Link>
-        </div>
-        <div className="gallery__item gallery__item--hor3 media-w-100 d-block">
+        </Link>
+        <Link to="/portfolio/location-sharing-sos-notifications-mobile-app" className="gallery__item gallery__item--hor3 media-w-100 d-block">
           <img src={webwork4} alt="location sharing and tracking with SOS calls"/>
           <Link to="/portfolio/location-sharing-sos-notifications-mobile-app"><div className="work-heading">SOS & Location Sharing Application </div>
           <div className="work-subheading">
           Location Sharing & Security · AI & ML · Live Tracking · SOS Mobile App 
           </div></Link>
-        </div>
-        <div className="gallery__item gallery__item--hor3 media-w-100 d-block">
+        </Link>
+        <Link to="/portfolio/pill-reminder-doctor-appointment-platform" className="gallery__item gallery__item--hor3 media-w-100 d-block">
           <img src={webwork5} alt="medicine and appointment reminder mobile app "/>
           <Link to="/portfolio/pill-reminder-doctor-appointment-platform"><div className="work-heading">Pill and Appointment Reminder Mobile App</div>
           <div className="work-subheading">
           Healthcare Technology · Custom Softwares · Medicine Reminder · Personalized Notifications
           </div></Link>
-        </div>
+        </Link>
       </div>
       <div className="gallery pb-md-5 pb-5 mobile-d-none">
-        <div className="gallery__item gallery__item--hor media-w-100 d-block">
+        <Link to="/portfolio/photo-sharing-online-ecommerce-platform" className="gallery__item gallery__item--hor media-w-100 d-block">
           <img src={webwork6} alt="image sharing and kids ecommerce app "/>
           <Link to="/portfolio/photo-sharing-online-ecommerce-platform"><div className="work-heading">Online Photo Sharing with E-Commerce </div>
           <div className="work-subheading">
           D2C & E-Commerce · Custom Softwares · Image Sharing · Payment Gateways
           </div></Link>
-        </div>
-        <div className="gallery__item media-w-100 d-block">
+        </Link>
+        <Link className="gallery__item media-w-100 d-block">
           <img src={work7} alt=""/>
          <Link><div className="work-heading">Architectural Drawing</div>
           <div className="work-subheading">
             D2C & E-commerce · Branding · UI Design · Marketing
           </div></Link> 
-        </div>
+        </Link>
       </div>
       <div className="gallery pb-md-5 pb-5 mobile-d-none">
-        <div className="gallery__item media-w-100 d-block">
+        <Link to="/portfolio/interview-scheduling-job-tracking-app" className="gallery__item media-w-100 d-block">
           <img src={webwork8} alt="interview scheduling and job tracking mobile app"/>
           <Link to="/portfolio/interview-scheduling-job-tracking-app"><div className="work-heading">Interview Scheduling & Job Tracking App</div>
           <div className="work-subheading">
           Staffing and Recruitment · Custom Softwares · Job Search Portal 
           </div></Link>
-        </div>
-        <div className="gallery__item gallery__item--hor2 media-w-100 d-block">
+        </Link>
+        <Link to="/portfolio/custom-school-learning-management-platform" className="gallery__item gallery__item--hor2 media-w-100 d-block">
           <img src={webwork9} alt="custom LMS softwares for schools"/>
           <Link to="/portfolio/custom-school-learning-management-platform"><div className="work-heading">Custom School Learning Platform </div>
           <div className="work-subheading">
           Educational Technology · AI & ML · Adaptive Modules · Accounting and Reporting
           </div>
           </Link>
-        </div>
+        </Link>
       </div>
       {/* Mobile mode */}
       <div className="desktop-d-none">
@@ -118,38 +118,38 @@ function Work() {
         </div>
         <div className="pb-5">
          <Link to="/portfolio/p2p-money-remittance-mobile-application"><img src={mobwork2} className="w-100" alt="peer to peer money remittance app "/>
-          <div className="work-heading">P2P Money Remittance Mobile Application </div></Link> 
+          <div className="work-heading">P2P Money Remittance Mobile Application </div>
           <div className="work-subheading">
           Banking & Finance · Mobile Apps · P2P Payments 
-          </div>
+          </div></Link> 
         </div>
         <div className="pb-5">
           <Link to="/portfolio/cab-hailing-travel-itinerary-platform"><img src={mobmork3} className="w-100" alt="Travel and itinerary app with cab booking"/>
-          <div className="work-heading">Travel & Cab Hailing Platform </div></Link>
+          <div className="work-heading">Travel & Cab Hailing Platform </div>
           <div className="work-subheading">
           Travel & Hospitality · Custom Softwares · Cab Hailing Platform
-          </div>
+          </div></Link>
         </div>
         <div className="pb-5">
          <Link to="/portfolio/location-sharing-sos-notifications-mobile-app"><img src={mobwork4} className="w-100" alt="location sharing and tracking with SOS calls "/>
-          <div className="work-heading">SOS & Location Sharing Application </div></Link> 
+          <div className="work-heading">SOS & Location Sharing Application </div>
           <div className="work-subheading">
           Location Sharing & Security · AI & ML · Live Tracking · SOS Mobile App 
-          </div>
+          </div></Link> 
         </div>
         <div className="pb-5">
         <Link to="/portfolio/pill-reminder-doctor-appointment-platform">  <img src={mobwork5} className="w-100" alt="medicine and appointment reminder mobile app "/>
-          <div className="work-heading">Pill and Appointment Reminder Mobile App</div></Link>
+          <div className="work-heading">Pill and Appointment Reminder Mobile App</div>
           <div className="work-subheading">
           Healthcare Technology · Custom Softwares · Medicine Reminder · Personalized Notifications
-          </div>
+          </div></Link>
         </div>
         <div className="pb-5">
           <Link to="/portfolio/photo-sharing-online-ecommerce-platform"><img src={mobwork6} className="w-100" alt="image sharing and kids ecommerce app "/>
-          <div className="work-heading">Online Photo Sharing with E-Commerce </div></Link>
+          <div className="work-heading">Online Photo Sharing with E-Commerce </div>
           <div className="work-subheading">
           D2C & E-Commerce · Custom Softwares · Image Sharing · Payment Gateways
-          </div>
+          </div></Link>
         </div>
         <div className="pb-5">
         <Link><img src={work7} className="w-100" />
