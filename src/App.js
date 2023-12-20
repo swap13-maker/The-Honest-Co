6851fa513fc30de71b56b5198bafbd527dae529f
surajpaul2412 <suraj.paul.69@gmail.com>
2023-12-20 06:45:04 +0530

    20

--- src/App.js
+++ src/App.js
@@ -27,40 +27,54 @@ import Project5 from './components/Portfolio/Project5.js';
 import Project6 from './components/Portfolio/Project6';
 import Project8 from './components/Portfolio/Project8';
 import Project9 from './components/Portfolio/Project9';
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/get-in-touch/customer-inquiry" element={<CustomerContact />} />
-        <Route path="/get-in-touch/creator-inquiry" element={<CreatorContact />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/resources" element={<Resource />} />
-        <Route path="/portfolio/online-d2c-ecommerce-store" element={<Project1 />} />
-        <Route path="/portfolio/p2p-money-remittance-mobile-application" element={<Project2 />} />
-        <Route path="/portfolio/cab-hailing-travel-itinerary-platform" element={<Project3 />} />
-        <Route path="/portfolio/location-sharing-sos-notifications-mobile-app" element={<Project4 />} />
-        <Route path="/portfolio/pill-reminder-doctor-appointment-platform" element={<Project5 />} />
-        <Route path="/portfolio/photo-sharing-online-ecommerce-platform" element={<Project6 />} />
-        <Route path="/portfolio/interview-scheduling-job-tracking-app" element={<Project8 />} />
-        <Route path="/portfolio/custom-school-learning-management-platform" element={<Project9 />} />
-        <Route path="/resources/articles/global-commerce-remittance-mobile-applications-business-expansion" element={<Article1 />} />
-        <Route path="/resources/articles/artificial-intelligence-machine-learning-crypto-trading-bot" element={<Article2 />} />
-        <Route path="/resources/articles/leveraging-ai-ml-models-farmers-early-detect-plant-diseases" element={<Article3 />} />
-        <Route path="/resources/articles/simplifying-ecommerce-store-creation-low-code-platforms" element={<Article4 />} />
-        <Route path="/resources/articles/optimizing-forecasting-customer-targeting-ai-ml-solutions" element={<Article5 />} />
-        <Route path="/resources/articles/comprehensive-preschool-mobile-application-parental-tracking-transits" element={<Article6 />} />
-        <Route path="/resources/articles/curating-adaptive-course-modules-deep-learning-algorithms" element={<Article7 />} />
-        <Route path="/resources/articles/contract-management-modules-automating-contracts-custom-app-development" element={<Article8 />} />
-        <Route path="/resources/articles/modernizing-interview-scheduling-mobile-apps-enhancing-team-efficiency-applicant-experience" element={<Article9 />} />
-        <Route path="/resources/articles/machine-learning-models-streamlining-application-filterization-shortlisting-hr-operations" element={<Article10 />} />
-        <Route path="/resources/articles/custom-crm-development-ecommerce-industries-businesses" element={<Article11 />} />
-        <Route path="/resources/articles/effective-route-planning-fleet-management-airiticifal-intelligence-models" element={<Article12 />} />
-        <Route path="/thankyou" element={<Thanks />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <>
+      <Helmet>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q0S7PV44D7"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Q0S7PV44D7');
+          `}
+        </script>
+      </Helmet>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/get-in-touch/customer-inquiry" element={<CustomerContact />} />
+          <Route path="/get-in-touch/creator-inquiry" element={<CreatorContact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/resources" element={<Resource />} />
+          <Route path="/portfolio/online-d2c-ecommerce-store" element={<Project1 />} />
+          <Route path="/portfolio/p2p-money-remittance-mobile-application" element={<Project2 />} />
+          <Route path="/portfolio/cab-hailing-travel-itinerary-platform" element={<Project3 />} />
+          <Route path="/portfolio/location-sharing-sos-notifications-mobile-app" element={<Project4 />} />
+          <Route path="/portfolio/pill-reminder-doctor-appointment-platform" element={<Project5 />} />
+          <Route path="/portfolio/photo-sharing-online-ecommerce-platform" element={<Project6 />} />
+          <Route path="/portfolio/interview-scheduling-job-tracking-app" element={<Project8 />} />
+          <Route path="/portfolio/custom-school-learning-management-platform" element={<Project9 />} />
+          <Route path="/resources/articles/global-commerce-remittance-mobile-applications-business-expansion" element={<Article1 />} />
+          <Route path="/resources/articles/artificial-intelligence-machine-learning-crypto-trading-bot" element={<Article2 />} />
+          <Route path="/resources/articles/leveraging-ai-ml-models-farmers-early-detect-plant-diseases" element={<Article3 />} />
+          <Route path="/resources/articles/simplifying-ecommerce-store-creation-low-code-platforms" element={<Article4 />} />
+          <Route path="/resources/articles/optimizing-forecasting-customer-targeting-ai-ml-solutions" element={<Article5 />} />
+          <Route path="/resources/articles/comprehensive-preschool-mobile-application-parental-tracking-transits" element={<Article6 />} />
+          <Route path="/resources/articles/curating-adaptive-course-modules-deep-learning-algorithms" element={<Article7 />} />
+          <Route path="/resources/articles/contract-management-modules-automating-contracts-custom-app-development" element={<Article8 />} />
+          <Route path="/resources/articles/modernizing-interview-scheduling-mobile-apps-enhancing-team-efficiency-applicant-experience" element={<Article9 />} />
+          <Route path="/resources/articles/machine-learning-models-streamlining-application-filterization-shortlisting-hr-operations" element={<Article10 />} />
+          <Route path="/resources/articles/custom-crm-development-ecommerce-industries-businesses" element={<Article11 />} />
+          <Route path="/resources/articles/effective-route-planning-fleet-management-airiticifal-intelligence-models" element={<Article12 />} />
+          <Route path="/thankyou" element={<Thanks />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
